198f6d03735d5312b4fdca599226b9cf712eb8cb
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-06 15:59:53 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/devalex/mongo/ProjapirestfulMongoApplication.java
@@ -0,0 +1,13 @@
+package com.devalex.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjapirestfulMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjapirestfulMongoApplication.class, args);
+	}
+
+}
