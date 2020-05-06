ce7a289513db918cabe70767123b8564edaca230
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-06 17:57:37 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/devalex/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.devalex.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
